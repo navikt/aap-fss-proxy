69a7dbb5ee3d278bfbfdc8fa709a8b12c6713287
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-11-18 11:37:43 +0100

    log

--- src/main/kotlin/no/nav/aap/joark/JoarkController.kt
+++ src/main/kotlin/no/nav/aap/joark/JoarkController.kt
@@ -3,6 +3,7 @@ package no.nav.aap.joark
 import no.nav.aap.config.SecurityConfig.Companion.ISSUER_AAD
 import no.nav.aap.joark.domain.Journalpost
 import no.nav.security.token.support.spring.ProtectedRestController
+import org.slf4j.LoggerFactory.getLogger
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 
@@ -10,8 +11,11 @@ import org.springframework.web.bind.annotation.RequestBody
 @ProtectedRestController(value = ["/joark"], issuer = ISSUER_AAD, claimMap =[""] )
 class JoarkController(private val joarkClient: JoarkClient) {
 
+    private val log = getLogger(javaClass)
+
     @PostMapping("/aad")
     fun opprettJournalpostFraSaksbehandler(@RequestBody journalpost: Journalpost) {
+        log.info("WOHOO, oppretter journalpost $journalpost")
         joarkClient.opprettJournalpost(journalpost)
     }
 }
