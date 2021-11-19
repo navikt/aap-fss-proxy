877951c51e8bfa2804191f192c47f010badc4c7b
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-11-19 11:26:30 +0100

    callid

--- src/main/kotlin/no/nav/aap/joark/JoarkClientConfig.kt
+++ src/main/kotlin/no/nav/aap/joark/JoarkClientConfig.kt
@@ -1,5 +1,6 @@
 package no.nav.aap.joark
 
+import no.nav.aap.api.util.MDCUtil
 import no.nav.aap.sts.StsClient
 import no.nav.boot.conditionals.ConditionalOnDevOrLocal
 import org.springframework.boot.actuate.trace.http.HttpTraceRepository
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpHeaders.AUTHORIZATION
 import org.springframework.web.reactive.function.client.ClientRequest
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction
+import org.springframework.web.reactive.function.client.ExchangeFunction
 import org.springframework.web.reactive.function.client.WebClient
 
 @Configuration
@@ -17,6 +19,7 @@ class JoarkClientConfig(private val webClientBuilder: WebClient.Builder, private
     fun joarkWebClient(stsClient: StsClient): WebClient {
         return webClientBuilder
             .baseUrl(joarkConfig.url)
+            .filter(correlatingFilterFunction())
             .filter(stsExchangeFilterFunction(stsClient))
             .build()
     }
@@ -25,6 +28,17 @@ class JoarkClientConfig(private val webClientBuilder: WebClient.Builder, private
     @ConditionalOnDevOrLocal
     fun httpTraceRepository(): HttpTraceRepository {
         return InMemoryHttpTraceRepository()
+    }
+
+    private fun correlatingFilterFunction() =
+         ExchangeFilterFunction { req: ClientRequest, next: ExchangeFunction ->
+            next.exchange(
+                ClientRequest.from(req)
+                    .header(MDCUtil.NAV_CONSUMER_ID, MDCUtil.consumerId())
+                    .header(MDCUtil.NAV_CALL_ID2, MDCUtil.callId())
+                    .build()
+            )
+        }
     }
 
     private fun stsExchangeFilterFunction(stsClient: StsClient) =
--- src/main/kotlin/no/nav/aap/util/MDCUtil.kt
+++ src/main/kotlin/no/nav/aap/util/MDCUtil.kt
@@ -7,6 +7,7 @@ object MDCUtil {
     const val NAV_CONSUMER_ID = "Nav-Consumer-Id"
     const val NAV_CALL_ID = "Nav-CallId"
     const val NAV_CALL_ID1 = "Nav-Call-Id"
+    const val NAV_CALL_ID2 = "callId"
     fun callId(): String {
         return MDC.get(NAV_CALL_ID) ?: UUID.randomUUID().toString()
     }
