e3825fbcfaac17429f2005db1bf78a5ee69feba3
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-11-19 16:24:29 +0100

    handle bad request

--- src/main/kotlin/no/nav/aap/error/ProxyExceptionHandler.kt
+++ src/main/kotlin/no/nav/aap/error/ProxyExceptionHandler.kt
@@ -6,12 +6,13 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.context.request.NativeWebRequest
-import org.springframework.web.reactive.function.client.WebClientResponseException
+import org.springframework.web.reactive.function.client.WebClientResponseException.
 import org.springframework.web.reactive.function.client.WebClientResponseException.*
 import org.zalando.problem.Problem
 import org.zalando.problem.Status.NOT_FOUND
 import org.zalando.problem.Status.UNAUTHORIZED
 import org.zalando.problem.Status.INTERNAL_SERVER_ERROR
+import org.zalando.problem.Status.BAD_REQUEST
 import org.zalando.problem.spring.web.advice.ProblemHandling
 
 @ControllerAdvice
@@ -25,6 +26,7 @@ class ProxyExceptionHandler: ProblemHandling {
     @ExceptionHandler(WebClientResponseException::class)
     fun handleWebClientResponseException(e: WebClientResponseException, req: NativeWebRequest): ResponseEntity<Problem> {
         return when (e) {
+            is BadRequest -> create(BAD_REQUEST,e,req)
             is Forbidden, is Unauthorized -> create(UNAUTHORIZED,e,req)
             is NotFound -> create(NOT_FOUND,e,req)
             else -> create(INTERNAL_SERVER_ERROR,e,req)
