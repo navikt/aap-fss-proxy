fe14b2eb303bc3503d68263c5b7055322f2e44c7
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-11-19 16:14:46 +0100

    vi er ingen oauth2 client

--- src/main/kotlin/no/nav/aap/ProxyApplication.kt
+++ src/main/kotlin/no/nav/aap/ProxyApplication.kt
@@ -1,23 +1,16 @@
 package no.nav.aap
 
-import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.retry.annotation.EnableRetry
 import no.nav.boot.conditionals.Cluster.profiler
-import no.nav.boot.conditionals.ConditionalOnDevOrLocal
-import no.nav.boot.conditionals.ConditionalOnNotProd
-import org.springframework.boot.actuate.trace.http.HttpTraceRepository
-import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository
-import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EnableRetry
 @EnableJwtTokenValidation(ignore = ["springfox.documentation", "org.springframework"])
-@EnableOAuth2Client(cacheEnabled = true)
 class ProxyApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/no/nav/aap/config/SecurityConfig.kt
+++ src/main/kotlin/no/nav/aap/config/SecurityConfig.kt
@@ -1,7 +1,5 @@
 package no.nav.aap.config
 
-import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
-import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.springframework.context.annotation.Configuration
 
 
