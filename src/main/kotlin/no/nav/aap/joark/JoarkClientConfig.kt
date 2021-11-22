cec5d3d2c62d775c37953ddafbf380aa8b7d0ba2
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-11-22 18:18:19 +0100

    qalifier

--- src/main/kotlin/no/nav/aap/joark/JoarkClientConfig.kt
+++ src/main/kotlin/no/nav/aap/joark/JoarkClientConfig.kt
@@ -1,5 +1,6 @@
 package no.nav.aap.joark
 
+import no.nav.aap.config.Constants
 import no.nav.aap.util.MDCUtil
 import no.nav.aap.sts.StsClient
 import no.nav.boot.conditionals.ConditionalOnDevOrLocal
@@ -11,11 +12,13 @@ import org.springframework.http.HttpHeaders.AUTHORIZATION
 import org.springframework.web.reactive.function.client.ClientRequest
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction
 import no.nav.aap.rest.AbstractRestConfig.Companion.correlatingFilterFunction
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.web.reactive.function.client.WebClient
 
 @Configuration
 class JoarkClientConfig(private val webClientBuilder: WebClient.Builder, private val cfg: JoarkConfig) {
     @Bean
+    @Qualifier(Constants.JOARK)
     fun joarkWebClient(stsClient: StsClient): WebClient {
         return webClientBuilder
             .baseUrl(cfg.baseUri.toString())
