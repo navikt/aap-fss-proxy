2b2c0b8f2e82f687426ceca12f5bdcb02221e0f4
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-11-22 19:03:44 +0100

    baseuri

--- src/main/kotlin/no/nav/aap/sts/StsConfig.kt
+++ src/main/kotlin/no/nav/aap/sts/StsConfig.kt
@@ -7,4 +7,4 @@ import org.springframework.boot.context.properties.bind.DefaultValue
 import java.net.URI
 
 @ConfigurationProperties(prefix = "sts")
-class StsConfig @ConstructorBinding constructor(uri: URI,path: String,@DefaultValue("true") enabled: Boolean): AbstractRestConfig(uri,path,enabled)
+class StsConfig @ConstructorBinding constructor(baseUri: URI,path: String,@DefaultValue("true") enabled: Boolean): AbstractRestConfig(baseUri,path,enabled)
