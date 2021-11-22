bf0e1c4d6bd0e146e6b31bc44f26977141343fe1
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-11-22 18:32:57 +0100

    secondart ctr

--- src/main/kotlin/no/nav/aap/sts/StsClient.kt
+++ src/main/kotlin/no/nav/aap/sts/StsClient.kt
@@ -4,7 +4,7 @@ import no.nav.aap.rest.RetryAware
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 @Component
-class StsClient constructor (private val adapter: StsWebClientAdapter): RetryAware {
-    internal constructor(webClient: WebClient, cfg: StsConfig) : this(StsWebClientAdapter(webClient,cfg))
+class StsClient (private val adapter: StsWebClientAdapter): RetryAware {
+    constructor(webClient: WebClient, cfg: StsConfig) : this(StsWebClientAdapter(webClient,cfg))
     fun oidcToken() = adapter.oidcToken();
 }
