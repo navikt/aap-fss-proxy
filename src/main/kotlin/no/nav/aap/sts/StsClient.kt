5307cae48a6d75c836ebf23c25dfcd1412ae252b
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-11-22 17:41:25 +0100

    fix test

--- src/main/kotlin/no/nav/aap/sts/StsClient.kt
+++ src/main/kotlin/no/nav/aap/sts/StsClient.kt
@@ -1,13 +1,10 @@
 package no.nav.aap.sts
 
 import no.nav.aap.rest.RetryAware
-import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
-import org.springframework.web.reactive.function.client.ClientResponse
 import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.reactive.function.client.bodyToMono
-
 @Component
-class StsClient(private val adapter: StsWebClientAdapter): RetryAware {
+class StsClient constructor (private val adapter: StsWebClientAdapter): RetryAware {
+    internal constructor(webClient: WebClient, cfg: StsConfig) : this(StsWebClientAdapter(webClient,cfg))
     fun oidcToken() = adapter.oidcToken();
 }
--- src/test/kotlin/no/nav/aap/sts/StsClientTest.kt
+++ src/test/kotlin/no/nav/aap/sts/StsClientTest.kt
@@ -5,35 +5,11 @@ import no.nav.aap.createShortCircuitWebClientQueued
 import org.intellij.lang.annotations.Language
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import java.net.URI
 
 class StsClientTest {
 
-    @Test
-    fun `henter ut riktig token`() {
-        val stsClient = StsClient(createShortCircuitWebClient(defaultToken))
-        val token = stsClient.oidcToken()
-
-        assertEquals("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJOQVYiLCJpYXQiOjE2MzYzNjkzMDMsImV4cCI6MTY2NzkwNTMwMywiYXVkIjoid3d3Lm5hdi5ubyIsInN1YiI6Im5hdkBuYXYuY29tIn0.Z2PGP0ATOGwzklthb2umsbnI2CHx1HPPDddIpnyKw8c", token)
-    }
-
-    @Test
-    fun `refresh token riktig`() {
-        val stsDefaultClient = StsClient(createShortCircuitWebClientQueued(shortLivedToken, defaultToken))
-        val token1 = stsDefaultClient.oidcToken()
-        val token2 = stsDefaultClient.oidcToken()
-
-        assertEquals("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJOQVYgdGVzdCIsImlhdCI6MTYzNjM2NjgxOCwiZXhwIjoxNjY3OTAyODE4LCJhdWQiOiJ3d3cubmF2Lm5vIiwic3ViIjoiMTExMTExMTExMSJ9.eTGkBsYMNKY7N9wX9xnY6jUTOSat4oxPEWU4wCeYsHQ", token1)
-        assertEquals("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJOQVYiLCJpYXQiOjE2MzYzNjkzMDMsImV4cCI6MTY2NzkwNTMwMywiYXVkIjoid3d3Lm5hdi5ubyIsInN1YiI6Im5hdkBuYXYuY29tIn0.Z2PGP0ATOGwzklthb2umsbnI2CHx1HPPDddIpnyKw8c", token2)
-    }
-
-    @Test
-    fun `token blir cachet`() {
-        val stsDefaultClient = StsClient(createShortCircuitWebClientQueued(defaultToken, shortLivedToken))
-        val token1 = stsDefaultClient.oidcToken()
-        val token2 = stsDefaultClient.oidcToken()
-
-        assertEquals(token1, token2)
-    }
+    val cfg = StsConfig(URI.create("http://localhost"),"path",true);
 
     @Language("json")
     private val defaultToken = """
@@ -52,4 +28,30 @@ class StsClientTest {
           "expires_in": 1
         }
     """.trimIndent()
+
+    @Test
+    fun `henter ut riktig token`() {
+        val stsClient = StsClient(createShortCircuitWebClient(defaultToken),cfg)
+        val token = stsClient.oidcToken()
+        assertEquals("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJOQVYiLCJpYXQiOjE2MzYzNjkzMDMsImV4cCI6MTY2NzkwNTMwMywiYXVkIjoid3d3Lm5hdi5ubyIsInN1YiI6Im5hdkBuYXYuY29tIn0.Z2PGP0ATOGwzklthb2umsbnI2CHx1HPPDddIpnyKw8c", token)
+    }
+
+
+    @Test
+    fun `refresh token riktig`() {
+        val stsDefaultClient = StsClient(createShortCircuitWebClientQueued(shortLivedToken, defaultToken),cfg)
+        val token1 = stsDefaultClient.oidcToken()
+        val token2 = stsDefaultClient.oidcToken()
+
+        assertEquals("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJOQVYgdGVzdCIsImlhdCI6MTYzNjM2NjgxOCwiZXhwIjoxNjY3OTAyODE4LCJhdWQiOiJ3d3cubmF2Lm5vIiwic3ViIjoiMTExMTExMTExMSJ9.eTGkBsYMNKY7N9wX9xnY6jUTOSat4oxPEWU4wCeYsHQ", token1)
+        assertEquals("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJOQVYiLCJpYXQiOjE2MzYzNjkzMDMsImV4cCI6MTY2NzkwNTMwMywiYXVkIjoid3d3Lm5hdi5ubyIsInN1YiI6Im5hdkBuYXYuY29tIn0.Z2PGP0ATOGwzklthb2umsbnI2CHx1HPPDddIpnyKw8c", token2)
+    }
+
+    @Test
+    fun `token blir cachet`() {
+        val stsDefaultClient = StsClient(createShortCircuitWebClientQueued(defaultToken, shortLivedToken),cfg)
+        val token1 = stsDefaultClient.oidcToken()
+        val token2 = stsDefaultClient.oidcToken()
+        assertEquals(token1, token2)
+    }
 }
