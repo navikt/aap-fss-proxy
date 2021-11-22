de40148441b3dd40f194e9834834ffb3a6a794da
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-11-22 18:56:37 +0100

    inject

--- src/main/kotlin/no/nav/aap/sts/StsClient.kt
+++ src/main/kotlin/no/nav/aap/sts/StsClient.kt
@@ -3,8 +3,10 @@ package no.nav.aap.sts
 import no.nav.aap.rest.RetryAware
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
+import javax.inject.Inject
+
 @Component
-class StsClient (private val adapter: StsWebClientAdapter): RetryAware {
+class StsClient @Inject constructor(private val adapter: StsWebClientAdapter): RetryAware {
     constructor(webClient: WebClient, cfg: StsConfig) : this(StsWebClientAdapter(webClient,cfg))
     fun oidcToken() = adapter.oidcToken();
 }
