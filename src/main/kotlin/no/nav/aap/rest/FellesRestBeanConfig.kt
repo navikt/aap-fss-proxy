1bd905575619bf2b5ea6b9571362224560fee6f7
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-11-23 14:53:20 +0100

    kotlinModule

---
+++ src/main/kotlin/no/nav/aap/rest/FellesRestBeanConfig.kt
@@ -0,0 +1,23 @@
+package no.nav.aap.rest
+
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import no.nav.boot.conditionals.ConditionalOnDevOrLocal
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
+import org.zalando.problem.jackson.ProblemModule
+@Bean
+fun customizer(): Jackson2ObjectMapperBuilderCustomizer {
+    return Jackson2ObjectMapperBuilderCustomizer { b: Jackson2ObjectMapperBuilder ->
+        b.modules(ProblemModule(), JavaTimeModule(), KotlinModule())
+    }
+}
+@Bean
+@ConditionalOnDevOrLocal
+fun httpTraceRepository(): HttpTraceRepository {
+    return InMemoryHttpTraceRepository()
+}
