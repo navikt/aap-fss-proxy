bbb544659a3549a17a3b169e2c2ec34eecc3dc74
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-11-24 09:55:39 +0100

    chore

--- src/main/kotlin/no/nav/aap/rest/FellesRestBeanConfig.kt
+++ src/main/kotlin/no/nav/aap/rest/FellesRestBeanConfig.kt
@@ -6,11 +6,17 @@ import io.swagger.v3.oas.models.ExternalDocumentation
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.info.License
+import no.nav.boot.conditionals.ConditionalOnDevOrLocal
+import org.springframework.boot.actuate.trace.http.HttpExchangeTracer
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository
+import org.springframework.boot.actuate.web.trace.servlet.HttpTraceFilter
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
 import org.zalando.problem.jackson.ProblemModule
+import javax.servlet.ServletException
+import javax.servlet.http.HttpServletRequest
 
 
 @Configuration
@@ -31,5 +37,13 @@ class FellesRestBeanConfig {
                     .version("v0.0.1")
                     .license(License().name("MIT").url("http://nav.no"))
             )
+    }
+    @ConditionalOnDevOrLocal
+    class ActuatorIgnoringTraceRequestFilter(repository: HttpTraceRepository?, tracer: HttpExchangeTracer?) :
+        HttpTraceFilter(repository, tracer) {
+        @Throws(ServletException::class)
+        override fun shouldNotFilter(request: HttpServletRequest): Boolean {
+            return request.servletPath.contains("actuator") || request.servletPath.contains("swagger")
+        }
     }
 }
