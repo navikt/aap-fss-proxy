ff60de6cca4e8b4cef6b6c1a1ed791c7594d607b
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-03-17 14:50:53 +0100

    url erg

--- src/main/kotlin/no/nav/aap/proxy/inntektskomponent/InntektskomponentWebClientAdapter.kt
+++ src/main/kotlin/no/nav/aap/proxy/inntektskomponent/InntektskomponentWebClientAdapter.kt
@@ -1,11 +1,13 @@
 package no.nav.aap.proxy.inntektskomponent
 
+import net.logstash.logback.composite.loggingevent.LoggingEventThreadNameJsonProvider
 import no.nav.aap.rest.AbstractWebClientAdapter
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.MediaType.*
 import no.nav.aap.util.Constants.INNTEKTSKOMPONENT
+import no.nav.aap.util.LoggerUtil
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.ClientResponse
 import org.springframework.web.reactive.function.client.WebClient
--- src/main/kotlin/no/nav/aap/proxy/organisasjon/OrganisasjonWebClientAdapter.kt
+++ src/main/kotlin/no/nav/aap/proxy/organisasjon/OrganisasjonWebClientAdapter.kt
@@ -2,6 +2,7 @@ package no.nav.aap.proxy.organisasjon
 
 import no.nav.aap.proxy.organisasjon.OrganisasjonConfig.Companion.ORGANISASJON
 import no.nav.aap.rest.AbstractWebClientAdapter
+import no.nav.aap.util.LoggerUtil
 import org.apache.commons.lang3.StringUtils.capitalize
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.cache.annotation.Cacheable
@@ -12,10 +13,11 @@ import java.util.*
 
 @Component
 class OrganisasjonWebClientAdapter(@Qualifier(ORGANISASJON)  val client: WebClient, private val cf: OrganisasjonConfig) : AbstractWebClientAdapter(client, cf) {
+    private val log = LoggerUtil.getLogger(javaClass)
 
-    @Cacheable(cacheNames = ["organisasjon"])
-    fun orgNavn(orgnr: String)  =
-             webClient
+    //@Cacheable(cacheNames = ["organisasjon"])
+    fun orgNavn(orgnr: String) : String {
+             var n = webClient
                 .get()
                 .uri { b -> cf.getOrganisasjonURI(b, orgnr) }
                 .accept(APPLICATION_JSON)
@@ -23,6 +25,11 @@ class OrganisasjonWebClientAdapter(@Qualifier(ORGANISASJON)  val client: WebClie
                 .toEntity(OrganisasjonDTO::class.java)
                 .block()
                 ?.body
-                ?.fulltNavn ?: orgnr
+
+                log.trace("Orgnavn $n")
+                val fn =  n?.fulltNavn ?: orgnr
+                log.trace("Orgnavn fullt $fn")
+                return fn
+    }
     override fun name() =  capitalize(ORGANISASJON.lowercase(Locale.getDefault()))
 }
