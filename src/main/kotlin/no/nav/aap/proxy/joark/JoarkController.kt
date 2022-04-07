49ffb149a1bd5893a9a72aca816c8f795ce8e5e4
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-04-07 11:48:21 +0200

    joark idporten

--- src/main/kotlin/no/nav/aap/proxy/joark/JoarkController.kt
+++ src/main/kotlin/no/nav/aap/proxy/joark/JoarkController.kt
@@ -2,14 +2,16 @@ package no.nav.aap.proxy.joark
 
 import no.nav.aap.joark.JoarkResponse
 import no.nav.aap.joark.Journalpost
+import no.nav.aap.util.Constants
 import no.nav.aap.util.Constants.AAD
+import no.nav.aap.util.Constants.IDPORTEN
 import no.nav.security.token.support.spring.ProtectedRestController
 import org.slf4j.LoggerFactory.getLogger
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 
 
-@ProtectedRestController(value = ["/joark"], issuer = AAD, claimMap =[""])
+@ProtectedRestController(value = ["/joark"], issuer = IDPORTEN)
 class JoarkController(private val joark: JoarkClient) {
 
     @PostMapping("/aad")
