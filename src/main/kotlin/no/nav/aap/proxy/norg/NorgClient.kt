33ef514df8a41c653056998f0e9caaf6a551b4c1
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-05-20 18:26:22 +0200

    fjernet norg

--- src/main/kotlin/no/nav/aap/proxy/norg/NorgClient.kt
+++
@@ -1,8 +0,0 @@
-package no.nav.aap.proxy.norg
-
-import org.springframework.stereotype.Component
-
-@Component
-class NorgClient(private val a: NorgWebClientAdapter) {
-    fun hentArbeidsfordeling(req: ArbeidRequest)  = a.hentArbeidsfordeling(req)
-}
