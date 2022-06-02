a5c65a7fbbd9376fe83997558e22f8b4e5d440fa
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-06-02 11:26:09 +0200

    fjerner ereg, er nå tilgjengelig fra gcp

--- src/main/kotlin/no/nav/aap/proxy/organisasjon/OrganisasjonClient.kt
+++
@@ -1,9 +0,0 @@
-package no.nav.aap.proxy.organisasjon
-
-import no.nav.aap.api.felles.OrgNummer
-import org.springframework.stereotype.Component
-
-@Component
-class OrganisasjonClient(private val a: OrganisasjonWebClientAdapter) {
-    fun orgNavn(orgnr: OrgNummer) = a.orgNavn(orgnr)
-}
