bff84cb4b50b8ca4d8443f541045c26157eb6d80
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-06-02 14:22:50 +0200

    log expiry

--- src/main/kotlin/no/nav/aap/proxy/sts/StsWebClientAdapter.kt
+++ src/main/kotlin/no/nav/aap/proxy/sts/StsWebClientAdapter.kt
@@ -26,7 +26,7 @@ class StsWebClientAdapter (@Qualifier(STS) webClient: WebClient, private val cf:
                 .doOnSuccess { log.trace("STS oppslag OK, utgår om ${it.expiresIn}s") }
                 .block()
         //}
-        val date = DateUtils.fromSecondsSinceEpoch(token?.accessToken?.jwtTokenClaims?.get("exp") as Long)
+        val date = token?.accessToken?.jwtTokenClaims?.get("exp")
         log.info("Token expiry at $date")
         log.info("Expires in ${token?.expiresIn}")
         return token!!.accessToken!!.tokenAsString
