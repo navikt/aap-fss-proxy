9da036ed7c3f807be65936374a05a644eef67298
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-06-03 11:42:33 +0200

    tidy

--- src/main/kotlin/no/nav/aap/proxy/sts/OidcToken.kt
+++ src/main/kotlin/no/nav/aap/proxy/sts/OidcToken.kt
@@ -1,5 +1,6 @@
 package no.nav.aap.proxy.sts
 
+import com.fasterxml.jackson.annotation.JsonAlias
 import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy
 import com.fasterxml.jackson.databind.annotation.JsonNaming
 import no.nav.aap.util.LoggerUtil
@@ -8,10 +9,10 @@ import java.time.LocalDateTime
 import java.time.ZoneId
 import java.util.Date
 
-@JsonNaming(SnakeCaseStrategy::class)
-  data class OidcToken(val accessToken: JwtToken? = null ,
-                       val tokenType: String? = null ,
-                       val expiresIn: Int? = null) {
+//@JsonNaming(SnakeCaseStrategy::class)
+  data class OidcToken(@JsonAlias("access_token") val accessToken: JwtToken? = null ,
+                       @JsonAlias("token_type") val tokenType: String? = null ,
+                       @JsonAlias("expires_in") val expiresIn: Int? = null) {
 
   val log = LoggerUtil.getLogger(javaClass)
     fun hasExpired() =
