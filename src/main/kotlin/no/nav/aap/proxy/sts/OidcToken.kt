1f2b08a77ce37f970d44c5828e1adfc1e8b6613e
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-06-03 11:39:13 +0200

    tidy

--- src/main/kotlin/no/nav/aap/proxy/sts/OidcToken.kt
+++ src/main/kotlin/no/nav/aap/proxy/sts/OidcToken.kt
@@ -4,7 +4,11 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy
 import com.fasterxml.jackson.databind.annotation.JsonNaming
 import no.nav.aap.util.LoggerUtil
 import no.nav.security.token.support.core.jwt.JwtToken
-import java.util.Date
+import sun.jvm.hotspot.code.CompressedStream.L
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.util.*
 
 @JsonNaming(SnakeCaseStrategy::class)
   data class OidcToken(val accessToken: JwtToken? = null ,
@@ -15,6 +19,8 @@ import java.util.Date
     fun hasExpired() =
        with(accessToken?.jwtTokenClaims?.get("exp") as Date) {
          Date().after(this).also {
+           val ldt = LocalDateTime.ofInstant(this.toInstant(), ZoneId.systemDefault())
+           log.info("LDT expiry $ldt, now er ${LocalDateTime.now()}")
            log.info("Token expiry at $this -> expired =  $it")
          }
        }
