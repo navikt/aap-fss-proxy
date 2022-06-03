b7ae93ae41e263f9d0baae8a9d79796a42f2072b
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-06-03 10:33:35 +0200

    log dates

--- src/main/kotlin/no/nav/aap/proxy/sts/OidcToken.kt
+++ src/main/kotlin/no/nav/aap/proxy/sts/OidcToken.kt
@@ -1,18 +1,23 @@
 package no.nav.aap.proxy.sts
 
-import com.fasterxml.jackson.annotation.JsonAlias
-import com.fasterxml.jackson.annotation.JsonCreator
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy
 import com.fasterxml.jackson.databind.annotation.JsonNaming
+import no.nav.aap.util.LoggerUtil
 import no.nav.security.token.support.core.jwt.JwtToken
-import java.time.LocalDateTime
-import java.time.LocalDateTime.*
+import java.util.Date
 
 @JsonNaming(SnakeCaseStrategy::class)
   data class OidcToken(val accessToken: JwtToken? = null ,
                        val tokenType: String? = null ,
                        val expiresIn: Int? = null) {
-    fun hasExpired() = now().plusSeconds(expiresIn!! - 20L).isBefore(now())
 
+  val log = LoggerUtil.getLogger(javaClass)
+    fun hasExpired(): Boolean {
+      //now().plusSeconds(expiresIn!! - 20L).isBefore(now())
+      val date = this.accessToken?.jwtTokenClaims?.get("exp") as Date
+      val now = Date()
+      val expired = now.after(date)
+      log.info("Token expiry at $date now er $now expired $expired")
+      return expired
+    }
 }
--- src/main/kotlin/no/nav/aap/proxy/sts/StsWebClientAdapter.kt
+++ src/main/kotlin/no/nav/aap/proxy/sts/StsWebClientAdapter.kt
@@ -6,40 +6,29 @@ import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToMono
-import java.time.LocalDateTime
-import java.time.ZoneId
-import java.time.ZoneOffset
-import java.util.*
 
 @Component
-class StsWebClientAdapter (@Qualifier(STS) webClient: WebClient, private val cf: StsConfig) : AbstractWebClientAdapter(webClient, cf) {
-    //private var token: OidcToken? = null
+class StsWebClientAdapter(@Qualifier(STS) webClient: WebClient, private val cf: StsConfig) :
+    AbstractWebClientAdapter(webClient, cf) {
+    private var token: OidcToken? = null
 
     fun oidcToken(): String {
-       // if (token.shouldBeRenewed()) {
-            val token = webClient.get()
-                .uri { b -> b.path(cf.tokenPath)
+        token.shouldBeRenewed())
+        token = webClient.get()
+            .uri { b ->
+                b.path(cf.tokenPath)
                     .queryParam("grant_type", "client_credentials")
                     .queryParam("scope", "openid")
                     .build()
-                }
-                .retrieve()
-                .bodyToMono<OidcToken>()
-                .doOnError { t: Throwable -> log.warn("STS oppslag feilet", t) }
-                .doOnSuccess { log.trace("STS oppslag OK, utgår om ${it.expiresIn}s") }
-                .block()
-        //}
-        val date = token?.accessToken?.jwtTokenClaims?.get("exp")
-        val now = Date()
-        log.info("Token expiry at $date now er $now")
+            }
+            .retrieve()
+            .bodyToMono<OidcToken>()
+            .doOnError { t: Throwable -> log.warn("STS oppslag feilet", t) }
+            .doOnSuccess { log.trace("STS oppslag OK, utgår om ${it.expiresIn}s") }
+            .block()
         return token!!.accessToken!!.tokenAsString
     }
 
-    fun convertToLocalDateViaInstant(d: Date): LocalDateTime {
-        return d.toInstant()
-            .atZone(ZoneId.of("GMT+1"))
-            .toLocalDateTime()
-    }
     override fun ping() {
         oidcToken()
     }
