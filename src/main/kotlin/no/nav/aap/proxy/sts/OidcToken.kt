f6c5f7e229d0294ccd41dde70c03dd8695e7e808
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-06-03 13:38:42 +0200

    tidy

--- src/main/kotlin/no/nav/aap/proxy/sts/OidcToken.kt
+++ src/main/kotlin/no/nav/aap/proxy/sts/OidcToken.kt
@@ -4,22 +4,21 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy
 import com.fasterxml.jackson.databind.annotation.JsonNaming
 import no.nav.aap.util.LoggerUtil
 import no.nav.security.token.support.core.jwt.JwtToken
-import java.time.LocalDateTime
-import java.time.ZoneId
+import java.time.LocalDateTime.now
+import java.time.LocalDateTime.ofInstant
+import java.time.ZoneId.systemDefault
 import java.util.Date
 
 @JsonNaming(SnakeCaseStrategy::class)
-  data class OidcToken(val accessToken: JwtToken? = null ,
-                       val tokenType: String? = null ,
+  data class OidcToken(val accessToken: JwtToken? = null,
+                       val tokenType: String? = null,
                        val expiresIn: Int? = null) {
 
   val log = LoggerUtil.getLogger(javaClass)
     fun hasExpired() =
-       with(accessToken?.jwtTokenClaims?.get("exp") as Date) {
-         Date().after(this).also {
-           val ldt = LocalDateTime.ofInstant(this.toInstant(), ZoneId.systemDefault())
-           log.info("LDT expiry $ldt, now er ${LocalDateTime.now()}")
-           log.info("Token expiry at $this -> expired =  $it")
+       with(ofInstant((accessToken?.jwtTokenClaims?.get("exp") as Date).toInstant(), systemDefault())) {
+         now().minusSeconds(30).isAfter(this).also {
+           log.info("${now().minusSeconds(30)} Token expiry at $this -> expired =  $it")
          }
        }
 }
