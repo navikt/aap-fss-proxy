3fa79134a2b2f1bf1560dc17f74a8909ca04a3f6
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-09-13 11:11:53 +0200

    fjernet joark-greier

--- src/main/kotlin/no/nav/aap/proxy/joark/JoarkClient.kt
+++
@@ -1,9 +0,0 @@
-package no.nav.aap.proxy.joark
-
-import no.nav.aap.joark.Journalpost
-import org.springframework.stereotype.Component
-@Component
-class JoarkClient(private val a: JoarkWebClientAdapter) {
-    fun opprettJournalpost(p: Journalpost)  = a.opprettJournalpost(p)
-    fun ping()= a.ping()
-}
