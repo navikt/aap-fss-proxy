73fcc8705025ad90a0f95a6e6ac4da1848fef2fa
Richard Borge <richard.borge@nav.no>
2023-01-13 09:17:01 +0100

    Legger inn nullable felter.

--- src/main/kotlin/no/nav/aap/proxy/arena/ArenaResponse.kt
+++ src/main/kotlin/no/nav/aap/proxy/arena/ArenaResponse.kt
@@ -1,9 +1,9 @@
 package no.nav.aap.proxy.arena
 
 data class ArenaResponse (
-    val aapSak: AapSak,
-    val ytelsesvedtak: Ytelsesvedtak,
-        )
+    val aapSak: AapSak?,
+    val ytelsesvedtak: Ytelsesvedtak?,
+)
 
 data class AapSak(
     val saksnummer: String,
@@ -19,14 +19,14 @@ data class Ytelsesvedtak(
     val beslutningsdato: String,
     val status: String,
     val unntaksvedtak: String,
-    val sisteMeldekort: SisteMeldekort,
+    val sisteMeldekort: SisteMeldekort?,
     val meldedato: String,
     val utbetaling: String
 )
 
 data class Periode(
     val fom: String,
-    val tom: String,
+    val tom: String?,
 )
 
 data class SisteMeldekort(
