a8d7f73041a658a92b4458f5afd4880ce12aeb92
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-02-09 11:16:26 +0100

    fix rebase

--- src/main/kotlin/no/nav/aap/proxy/arena/ArenaClient.kt
+++ src/main/kotlin/no/nav/aap/proxy/arena/ArenaClient.kt
@@ -3,6 +3,53 @@ package no.nav.aap.proxy.arena
 import io.micrometer.observation.annotation.Observed
 import no.nav.aap.api.felles.Fødselsnummer
 import org.springframework.stereotype.Component
+<<<<<<< HEAD
+=======
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.bodyToMono
+import java.net.URI
+import java.util.*
+
+@ConfigurationProperties("arena")
+class ArenaConfig(baseUri: URI,
+                  @DefaultValue("/aap/sisteVedtak") val path: String,
+                  @DefaultValue("true") enabled: Boolean, retry: RetryConfig = RetryConfig.DEFAULT): AbstractRestConfig(baseUri,"",
+    "arena",enabled, retry)
+
+@Configuration
+class ArenaClientConfig(private val cfg: ArenaConfig) {
+
+    @Bean
+    @Qualifier("arena")
+    fun arenaWebClient(builder: WebClient.Builder, @Qualifier("arenaoidc") arenaExchangeFilterFunction: ExchangeFilterFunction) =
+        builder
+            .baseUrl("${cfg.baseUri}")
+            .filter(arenaExchangeFilterFunction)
+            .build()
+
+    @Bean
+    fun arenaHealthIndicator(a: StsWebClientAdapter) = object : AbstractPingableHealthIndicator(a) {}
+
+}
+
+@Component
+class ArenaWebClientAdapter(@Qualifier("arena") webClient: WebClient, private val cf: ArenaConfig) : AbstractWebClientAdapter(webClient, cf) {
+
+    fun getSisteVedtak(fnr: String) =
+        webClient
+            .get()
+            .uri { b -> b.path(cf.path).build() }
+            .header("fnr", fnr)
+            .retrieve()
+            .bodyToMono<ByteArray>()
+            .doOnError { t: Throwable -> log.warn("Arenaoppslag feilet", t) }
+            .doOnSuccess { log.trace("Arenaoppslag OK") }
+            .block()
+            .also { log.trace("Arena response $it") }
+}
+
+>>>>>>> 4c5fbbb (janoalav gjør ting han ikke kan)
 @Component
 @Observed
 class ArenaClient(private val a: ArenaWebClientRestAdapter) {
--- src/main/kotlin/no/nav/aap/proxy/arena/ArenaOIDCClient.kt
+++ src/main/kotlin/no/nav/aap/proxy/arena/ArenaOIDCClient.kt
@@ -1,6 +1,50 @@
 package no.nav.aap.proxy.arena
 
 import org.springframework.stereotype.Component
+<<<<<<< HEAD
+=======
+import org.springframework.web.reactive.function.client.ClientRequest
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.bodyToMono
+import java.net.URI
+import java.nio.charset.StandardCharsets
+import java.util.*
+import javax.inject.Inject
+import no.nav.aap.rest.AbstractRestConfig.RetryConfig.Companion.DEFAULT
+
+@ConfigurationProperties("arenaoidc")
+class ArenaOIDCConfig(baseUri: URI,
+                      @DefaultValue("true") enabled: Boolean, retry: RetryConfig = DEFAULT): AbstractRestConfig(baseUri,"",
+    "arenaoidc",enabled, retry)
+
+@ConfigurationProperties("arenaclient")
+data class ArenauserConfig (val id: String, val secret: String) {
+    val credentials = Base64.getEncoder().encodeToString("$id:$secret".toByteArray(StandardCharsets.UTF_8))
+}
+
+@Configuration
+class ArenaOIDCClientConfig(private val cfg: ArenauserConfig) {
+
+    @Bean
+    @Qualifier("arenaoidc")
+    fun arenaOIDCWebClient(builder: WebClient.Builder, arenaOIDCConfig: ArenaOIDCConfig) =
+        builder
+            .baseUrl("${arenaOIDCConfig.baseUri}")
+            .filter(arenaOIDCExchangeFilterFunction())
+            .build()
+
+    @Bean
+    fun arenaOIDCHealthIndicator(a: StsWebClientAdapter) = object : AbstractPingableHealthIndicator(a) {}
+
+    private fun arenaOIDCExchangeFilterFunction() =
+        ExchangeFilterFunction {
+                req, next -> next.exchange(
+            ClientRequest.from(req).header(HttpHeaders.AUTHORIZATION, "Basic ${cfg.credentials}")
+            .build())
+        }
+}
+>>>>>>> 4c5fbbb (janoalav gjør ting han ikke kan)
 
 @Component
 class ArenaOIDCClient (private val a: ArenaOIDCWebClientAdapter)  {
