6c95bae4a9523d012defee4c10d485acec32c218
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-02-14 13:36:30 +0100

    log

--- src/main/kotlin/no/nav/aap/proxy/arena/ArenaWebServiceAdapter.kt
+++ src/main/kotlin/no/nav/aap/proxy/arena/ArenaWebServiceAdapter.kt
@@ -6,7 +6,9 @@ import no.nav.aap.proxy.arena.generated.Bruker
 import no.nav.aap.proxy.arena.generated.HentSaksInfoListeRequestV2
 import no.nav.aap.proxy.arena.generated.HentSaksInfoListeV2Response
 import no.nav.aap.proxy.arena.generated.ObjectFactory
+import no.nav.aap.proxy.arena.generated.SaksInfo
 import no.nav.aap.util.Constants.AAP
+import no.nav.aap.util.LoggerUtil
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import org.springframework.ws.client.core.WebServiceOperations
@@ -14,19 +16,22 @@ import org.springframework.ws.client.core.WebServiceOperations
 @Component
 class ArenaWebServiceAdapter(private val operations: WebServiceOperations) {
 
-    fun hentSaker(fnr: Fødselsnummer) =
-         (operations.marshalSendAndReceive(request(fnr)) as HentSaksInfoListeV2Response).saksInfoListe.saksInfo
+    private val log = LoggerUtil.getLogger(javaClass)
+
+    fun hentSaker(fnr: Fødselsnummer): List<SaksInfo> {
+        log.info("Henter saker")
+        return  (operations.marshalSendAndReceive(request(fnr)) as HentSaksInfoListeV2Response).saksInfoListe.saksInfo
              .filter { it.tema.equals(AAP, ignoreCase = true) }
              .filter { it.sakstatus.equals("Aktiv",ignoreCase = true) }
              .filterNot { it.sakstypekode.equals("KLAN", ignoreCase = true) }
              .sortedByDescending { it.sakOpprettet.toLocalDateTime() }
-
+    }
     private fun request(fnr: Fødselsnummer)  =
         ObjectFactory().createHentSaksInfoListeV2(HentSaksInfoListeRequestV2()
             .apply {
             bruker = Bruker().apply {
                 brukerId = fnr.fnr
-                brukertypeKode = "PERSON"
+                brukertypeKode = PERSON
             }
             tema = AAP
             isLukket = false})
