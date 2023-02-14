cf80691e1c8ec183d114adc05de90074532d2197
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-02-14 22:54:59 +0100

    faultmessageresolver

--- src/main/kotlin/no/nav/aap/proxy/arena/ArenaBeanConfig.kt
+++ src/main/kotlin/no/nav/aap/proxy/arena/ArenaBeanConfig.kt
@@ -92,7 +92,7 @@ class ArenaBeanConfig {
         override fun handleFault(ctx: MessageContext): Boolean {
             with((ctx.response as SoapMessage).envelope.body) {
                 log.error("OOPS FSOR ${fault.faultStringOrReason}")
-                log.error("OOPS CoDE ${fault.faultCode}")
+                log.error("OOPS CODE ${fault.faultCode}")
                 fault.faultDetail.detailEntries.forEach { log.error("OOPS ENTRY $it") }
             }
             return false
