1ba7a757e5821a835ff3af0acb932c840d379f2e
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-02-15 16:12:31 +0100

    test

--- src/main/kotlin/no/nav/aap/proxy/arena/ArenaSoapAdapter.kt
+++ src/main/kotlin/no/nav/aap/proxy/arena/ArenaSoapAdapter.kt
@@ -20,19 +20,14 @@ class ArenaSoapAdapter(private val operations: WebServiceOperations, private val
 
     fun hentSaker(fnr: String) =
        // if (cfg.enabled) {
-            operations.marshalSendAndReceive(cfg.sakerURI,request(fnr)).also {
-                val r = it as JAXBElement<HentSaksInfoListeV2Response>
-                log.info("RES er${r.value}")
-            }// as? HentSaksInfoListeV2Response)?.saksInfoListe?.saksInfo
-            /*
+        (operations.marshalSendAndReceive(cfg.sakerURI,request(fnr)) as JAXBElement<HentSaksInfoListeV2Response>).value
+            ?.saksInfoListe?.saksInfo
                 ?.filter { it.tema.equals(AAP, ignoreCase = true) }
                 ?.filter { it.sakstatus.equals(AKTIV,ignoreCase = true) }
                 ?.filterNot { it.sakstypekode.equals(KLAGEANKE, ignoreCase = true) }
                 ?.sortedByDescending { it.sakOpprettet.toLocalDateTime() }.also {
                     log.info("Saker for $fnr er $it")
                 }
-                */
-
        // } else {
            // log.info("Oppslag saker disabled")
           // return  emptyList()
