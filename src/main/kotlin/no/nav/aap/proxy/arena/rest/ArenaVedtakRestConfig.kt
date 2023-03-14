835595cdabd13d487baf9c3a49cf8df693e7acfa
Jan-Olav Eide <jan.olav.eide@gmail.com>
2023-03-14 19:43:23 +0100

    ping ords

--- src/main/kotlin/no/nav/aap/proxy/arena/rest/ArenaVedtakRestConfig.kt
+++ src/main/kotlin/no/nav/aap/proxy/arena/rest/ArenaVedtakRestConfig.kt
@@ -14,8 +14,9 @@ import org.springframework.boot.context.properties.bind.DefaultValue
 class ArenaVedtakRestConfig(baseUri: URI,
                             @DefaultValue(SISTE_VEDTAK) val path: String,
                             @DefaultValue(TOKEN_PATH) val tokenPath: String,
+                            @DefaultValue(PING_PATH) pingPath: String,
                             @NestedConfigurationProperty val credentials: ArenaCredentials,
-                            @DefaultValue("true") enabled: Boolean): AbstractRestConfig(baseUri,"", ARENA,enabled, DEFAULT) {
+                            @DefaultValue("true") enabled: Boolean): AbstractRestConfig(baseUri,pingPath, ARENA,enabled, DEFAULT) {
 
     val asBasic =  "Basic ${"${credentials.id}:${credentials.secret}".encode()}"
 
@@ -23,6 +24,7 @@ class ArenaVedtakRestConfig(baseUri: URI,
     companion object {
         private const val TOKEN_PATH = "/oauth/token"
         const val SISTE_VEDTAK = "/v1/aap/sisteVedtak"
+        const val PING_PATH = "/v1/test/ping"
         const val ARENA = "arena"
         const val ARENAOIDC = "arenaoidc"
     }
