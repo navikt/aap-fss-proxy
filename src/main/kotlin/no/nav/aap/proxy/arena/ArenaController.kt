caa22936ca3915af8205eda244b750dd4ff7481e
Fredrik Meyer <fredrik.meyer@nav.no>
2025-05-28 13:39:04 +0200

    Noe logging

--- src/main/kotlin/no/nav/aap/proxy/arena/ArenaController.kt
+++ src/main/kotlin/no/nav/aap/proxy/arena/ArenaController.kt
@@ -1,21 +1,21 @@
 package no.nav.aap.proxy.arena
 
-import org.springframework.http.HttpStatus.CREATED
-import org.springframework.http.ResponseEntity.*
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.ResponseStatus
 import no.nav.aap.api.felles.Fødselsnummer
 import no.nav.aap.proxy.arena.soap.ArenaOpprettOppgaveParams
 import no.nav.aap.proxy.arena.soap.BehandleKjoerelisteOgOpprettOppgaveRequest
 import no.nav.aap.util.Constants.AAD
+import no.nav.aap.util.LoggerUtil.getLogger
 import no.nav.security.token.support.spring.ProtectedRestController
-import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.http.HttpStatus.CREATED
+import org.springframework.http.ResponseEntity
+import org.springframework.http.ResponseEntity.noContent
+import org.springframework.http.ResponseEntity.ok
+import org.springframework.web.bind.annotation.*
 
 @ProtectedRestController(value = ["/arena"], issuer = AAD, claimMap = [""])
 class ArenaController(private val arena : ArenaClient) {
+
+    private val log = getLogger(javaClass)
 
     @GetMapping("/vedtak")
     fun sisteVedtakNy(@RequestHeader("personident") personident: String) = arena.sisteVedtak(Fødselsnummer(personident))
@@ -29,7 +29,12 @@ class ArenaController(private val arena : ArenaClient) {
 
     @Deprecated("Fjern når disse ikke brukes i soknad-api pga fnr i path")
     @GetMapping("/nyesteaktivesak/{fnr}")
-    fun nyesteAktiveSak(@PathVariable fnr : Fødselsnummer) = arena.nyesteSak(fnr)?.let { ok(it) } ?: noContent().build()
+    fun nyesteAktiveSak(@PathVariable fnr: Fødselsnummer): ResponseEntity<String>  {
+        log.info("Henter nyeste aktive sak.")
+        val res = arena.nyesteSak(fnr)?.let { ok(it) } ?: noContent().build()
+        log.info("Hentet nyeste aktive sak. Respons: ${res.statusCode}")
+        return res
+    }
 
     @PostMapping("/opprettoppgave")
     @ResponseStatus(CREATED)
