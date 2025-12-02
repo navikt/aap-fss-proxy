8731d84ae8f3a638988744e515e4979ee6bf8b8f
Fredrik Meyer <fredrik.meyer@nav.no>
2025-12-02 12:21:26 +0100

    Ikke-fungerrende versjon, må testes mer

--- src/main/kotlin/no/nav/aap/util/WebClientExtensions.kt
+++ src/main/kotlin/no/nav/aap/util/WebClientExtensions.kt
@@ -7,16 +7,16 @@ import no.nav.aap.api.felles.error.RecoverableIntegrationException
 
 object WebClientExtensions {
 
-    inline fun <reified T> ClientResponse.response() =
-        with(statusCode()) {
-            if (is2xxSuccessful) {
+    inline fun <reified T : Any> ClientResponse.response() =
+        with(statusCode().value()) {
+            if (this in 200..299) {
                 bodyToMono(T::class.java)
             }
-            else if (is4xxClientError) {
-                Mono.error(IrrecoverableIntegrationException("$this"))
+            else if (this in 400..499) {
+                Mono.error(IrrecoverableIntegrationException("HTTP $this"))
             }
             else {
-                Mono.error(RecoverableIntegrationException("$this"))
+                Mono.error(RecoverableIntegrationException("HTTP $this"))
             }
         }
 }
