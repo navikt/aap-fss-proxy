eabd37eb8a614bb23899d6bdabe2bba326203ef9
Per Henden <per.christian.henden@nav.no>
2026-03-02 11:08:25 +0100

    Bump jib gradle-plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.7"
     kotlin("jvm") version "2.3.10"
     kotlin("plugin.spring") version "2.3.10"
-    id("com.google.cloud.tools.jib") version "3.5.1"
+    id("com.google.cloud.tools.jib") version "3.5.3"
 }
 
 group = "no.nav.aap"
