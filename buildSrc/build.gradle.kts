77fbdf8b0a0952152dad9c8693e96435da243ccd
Per Henden <per.christian.henden@nav.no>
2026-04-07 09:53:27 +0200

    Bruk aap-conventions plugin som de andre gradle-prosjektene i AAP
    AAPM-54

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,23 @@
+import org.gradle.kotlin.dsl.maven
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    maven("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.20")
+    implementation("dev.detekt:detekt-gradle-plugin:2.0.0-alpha.2")
+}
+
+kotlin {
+    jvmToolchain(21)
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_21)
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,27 @@
+import org.gradle.kotlin.dsl.maven
+
 rootProject.name = "aap-fss-proxy"
 
+val githubPassword: String? by settings
+
+dependencyResolutionManagement {
+    // Felles for alle gradle prosjekter i repoet
+    @Suppress("UnstableApiUsage")
+    repositories {
+        maven("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
+        mavenCentral()
+        maven("https://packages.confluent.io/maven/")
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/navikt/behandlingsflyt")
+            credentials {
+                username = "x-access-token"
+                password = (githubPassword
+                    ?: System.getenv("GITHUB_PASSWORD")
+                    ?: System.getenv("GITHUB_TOKEN")
+                    ?: error("requires either GITHUB_TOKEN or GITHUB_PASSWORD to be set in env"))
+            }
+        }
+        mavenLocal()
+    }
+}
